642e033e1675f20b325d1a99049db5868240a0c3
diego <diego.pedrazas@kenility.com>
2024-05-13 22:54:19 -0400

    poke api

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    @IsString()
+    @MinLength(2)
+    name: string;
+
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
+    no: number;
+}
